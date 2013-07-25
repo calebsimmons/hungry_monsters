fb8bbf6072517e3adb79f75552fd108104d0bf26
Alex Stachowiak <astac1@umbc.edu>
2013-07-25 14:07:44 -0400

    Python script to call simulation and find fitness.

---
+++ alon/simulate.py
@@ -0,0 +1,72 @@
+# simulate.py - Simulates and visualizes SBML-sh file.
+import sys
+import os
+import subprocess
+import tempfile
+import mod2sbml 
+from string import Template
+
+def simulate_model (param=None):
+    if param == None: 
+        # Open SBML-shorthand file.
+        sbml_sh_file = "alon.py"
+        try:
+            sbml_sh = open (sbml_sh_file, 'r').read()
+        except:
+            print "Error! Cannot open '{}'.".format (sbml_sh_file) 
+            sys.exit (1)
+    else:
+        # Open template and substitute values
+        sbml_template_file = "alon.template"
+        try:
+            sbml_template = open (sbml_template_file, 'r').read()
+            sbml_sh = Template (sbml_template).substitute ({
+                'K_xy': param[0],
+                'K_xz': param[1],
+                'K_yz': param[2]
+            })
+        except:
+            print "Error! Cannot open '{}'.".format (sbml_sh_file) 
+            sys.exit (1)
+
+    return simulate (sbml_sh)
+
+def simulate (sbml_sh):
+
+    # Convert to SBML and save in temporary file.
+    try:
+        SBML = mod2sbml.parse (sbml_sh)
+        temp = tempfile.NamedTemporaryFile(delete=False)
+        temp.write (SBML)
+        temp.flush()
+    except:
+        print "Error! Couldn't parse SBML-sh."
+        sys.exit (1)
+
+    # Run simulation.
+    command = """
+        simulateSBML -t {time} -s {steps} -l -m 3 {file_name}
+    """.format (
+        time=7200, 
+        steps=30,  
+        file_name=temp.name
+        ).strip()
+    subprocess.call (command.split (' '))
+
+    # out.csv
+    #print open ('out.csv').read()
+    ATP = map (float, open ('out.csv').readlines()[-1].split (","))
+    gained, spent = ATP [8], ATP [9]
+    fitness = gained - spent
+    
+    # Return fitness.
+    return fitness
+
+
+def main():
+
+    # Call simulation.
+    print "Fitness:", simulate_model ([10, 10, 10])
+
+if __name__ == "__main__":
+    main()
